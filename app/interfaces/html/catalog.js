c413539de2b76143c6e483e273810e7118ac95bc
Radioactive lab <37350375+imnetcat@users.noreply.github.com>
2020-12-09 10:14:45 -0800

    load catalog

--- app/interfaces/html/catalog.js
+++ app/interfaces/html/catalog.js
@@ -1,9 +1,32 @@
-async function LoadCatalog(){
-  let result = await makeRequest('', "POST", "/catalog");
-  result = JSON.parse(result);
-  const li = document.createElement("li");
-  for(const modl of result.res){
-    li.innerHTML += `<a target="content" href="/modules/${modl}/${modl}.html"><span>${modl.charAt(0).toUpperCase() + modl.slice(1)}</span></a>`;
-  }
-  document.getElementById("menu-ul").prepend(li);
+const LoadCatalog = () => {
+	const promise = makeRequest("{}", "POST", "/catalog");
+	promise.then( (res) => {
+		if(res === undefined){
+			return;
+		}
+		const catalog = document.getElementById("catalog");
+		res = JSON.parse(res);
+		for(const film of res){
+			const html_film = `<div class="anime-column">
+				<a class="image-block" href="/player?id=${film.filmid}">
+					<span class="year-block">2007</span>
+					<img src="/posters/${film.poster}" alt="${film.title}">
+				</a>
+				<div class="anime-column-info">
+					<a class="anime-title" href="/player?id=${film.filmid}">${film.title}</a>
+					<div class="icons-row">
+						<div title="Количество просмотров"><i class="fa fa-eye"></i>${film.views}
+					</div>
+				</div>
+				<div class="rating-info" title=".хак//Вернувшийся">
+				<span class="fa fa-star rating-star" data-id="4124" data-modal="rating-modal"></span>
+						<span class="main-rating-block">
+							<span class="main-rating">${film.rating}</span>
+						</span>
+					</div>
+				</div>
+			</div>`;
+			catalog.appendChild(html_film);
+		}
+	}).catch((err)=>{});
 }
