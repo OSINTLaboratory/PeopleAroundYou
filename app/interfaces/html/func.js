4ef84becbcca9996801941e025014b5dc904734e
Radioactive lab <37350375+imnetcat@users.noreply.github.com>
2020-12-09 05:02:08 -0800

    olololo debuging

--- app/interfaces/html/func.js
+++ app/interfaces/html/func.js
@@ -3,8 +3,10 @@
 const IsLogin = async () => {
 	const promise = makeRequest("{}", "POST", "/islogin");
 	promise.then( (res) => {
+		console.dir(res);
 		if(res.bool){
+			console.log("ololololo")
 			document.querySelectorAll(".auth-block").forEach(el => el.remove());
 		}
-	});
+	}).catch(err){};
 }
