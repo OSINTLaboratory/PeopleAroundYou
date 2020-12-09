8e26bbb6d30b4cd1731361714277555ab7f7676e
Radioactive lab <37350375+imnetcat@users.noreply.github.com>
2020-12-09 15:47:43 -0800

    paginator created

--- app/interfaces/html/catalog.js
+++ app/interfaces/html/catalog.js
@@ -1,4 +1,4 @@
-
+'use strict';
 class Page{
 	constructor(arrayOfFilms){
 		this.data = arrayOfFilms;
@@ -6,6 +6,37 @@ class Page{
 };
 
 let filmCatalog = new Array;
+
+const ShowPage = (page) => {
+	if(filmCatalog[page] === undefined){
+		return;
+	}
+	
+	const catalog = document.getElementById("catalog");
+	catalog.innerHTML = '';
+	for(let film of filmCatalog[page].data){
+		catalog.appendChild(film);
+	}
+	
+	// Set up pages pagination
+	const pages = document.getElementById("pagination");
+	pages.innerHTML = '';
+	const active_li = document.createElement("li");
+	active_li.className = 'active';
+	let i = 0;
+	const len = filmCatalog.length;
+	while(i < len){
+		const li = document.createElement("li");
+		if(i === page){	
+			active_li.innerHTML = `<span>${i+1}</span>`;
+			pages.appendChild(active_li);
+		}else{
+			li.innerHTML = `<a onclick="ShowPage(${i})" style="cursor:pointer;">${i+1}</a>`;
+			pages.appendChild(li);
+		}
+		i++;
+	}
+}
 
 const LoadCatalog = () => {
 	const promise = makeRequest("{}", "POST", "/catalog");
@@ -33,41 +64,24 @@ const LoadCatalog = () => {
 				<div class="anime-column-info">
 					<a class="anime-title" href="/player?id=${film.filmid}">${film.title}</a>
 					<div class="icons-row">
-						<div title="Количество просмотров"><i class="fa fa-eye"></i>${film.views}
+						<div title="Количество просмотров"><i class="fa fa-eye"></i>${film.views}</div>
 					</div>
 				</div>
 				<div class="rating-info" title=".хак//Вернувшийся">
-				<span class="fa fa-star rating-star" data-id="4124" data-modal="rating-modal"></span>
-						<span class="main-rating-block">
-							<span class="main-rating">${film.rating}</span>
-						</span>
-					</div>
+					<span class="fa fa-star rating-star" data-id="4124" data-modal="rating-modal"></span>
+					<span class="main-rating-block">
+						<span class="main-rating">${film.rating}</span>
+					</span>
 				</div>`;
-			page.push(html_film);
+			page.data.push(html_film);
 			i++;
 		}
-		console.log(filmCatalog)
 		if(filmCatalog[0] === undefined){
 			return;
 		}
 		
 		// Show first page
-		const catalog = document.getElementById("catalog");
-		for(let film of filmCatalog[0].data){
-			catalog.appendChild(film);
-		}
+		ShowPage(0);
 		
-		// Set up pages
-		const pages = document.getElementById("pagination");
-		console.log(pages)
-		const li = document.createElement("li");
-		pages.innerHTML = '<li class="active"><span>1</span></li>';
-		i = 2;
-		console.log(filmCatalog)
-		for(let film of filmCatalog){
-			li.innerHTML = `<a href="/catalog?page=${i}">${i}</a>`;
-			catalog.appendChild(li);
-			i++;
-		}
 	}).catch((err)=>{});
 }
--- app/interfaces/http.js
+++ app/interfaces/http.js
@@ -38,11 +38,14 @@ class Http {
 			await query.exec((err, result) => {
 				if(err){
 					Core.log.warning(err);
-					res.status(500).end();
-					return;
+					//res.status(500).end();
+					//return;
 				}
 				const stringified  = JSON.stringify(result);
-				res.send(stringified).end();
+				
+				// test data
+				const r = [{"filmid":1,"title":"Bitcoin","year":2017,"rating":"5.45","views":300469,"poster":"Bitcoin.png","genre":4},{"filmid":2,"title":"Bridge","year":2015,"rating":"7.25","views":76489,"poster":"Bridge.png","genre":2},{"filmid":3,"title":"Buildings","year":2002,"rating":"8.90","views":103538,"poster":"Buildings.png","genre":2},{"filmid":4,"title":"Car","year":2020,"rating":"9.56","views":3235900,"poster":"Car.png","genre":4},{"filmid":5,"title":"Cherry","year":2011,"rating":"8.98","views":7853234,"poster":"Cherry.png","genre":2},{"filmid":6,"title":"City","year":204,"rating":"5.02","views":4520683,"poster":"City.png","genre":2},{"filmid":7,"title":"Coffee","year":2020,"rating":"9.99","views":7099098,"poster":"Coffee.png","genre":3},{"filmid":8,"title":"Computer","year":1998,"rating":"3.67","views":367833,"poster":"Computer.png","genre":5},{"filmid":9,"title":"Design","year":2004,"rating":"4.39","views":46368008,"poster":"Design.png","genre":5},{"filmid":10,"title":"Dubai","year":2015,"rating":"8.77","views":52453,"poster":"Dubai.png","genre":2},{"filmid":11,"title":"Fire","year":2012,"rating":"4.91","views":34687900,"poster":"Fire.png","genre":1},{"filmid":12,"title":"Futuristic","year":2013,"rating":"6.72","views":12319056,"poster":"Futuristic.png","genre":4},{"filmid":13,"title":"Glitch","year":2020,"rating":"7.68","views":7099098,"poster":"Glitch.png","genre":5},{"filmid":14,"title":"Hud","year":2015,"rating":"5.48","views":5063452,"poster":"Hud.png","genre":5},{"filmid":15,"title":"Jellyfish","year":2001,"rating":"4.70","views":340657,"poster":"Jellyfish.png","genre":1},{"filmid":16,"title":"Landing","year":2018,"rating":"7.80","views":530034,"poster":"Landing.png","genre":2},{"filmid":17,"title":"Loading","year":2010,"rating":"8.60","views":102405,"poster":"Loading.png","genre":5},{"filmid":18,"title":"Music","year":2002,"rating":"9.50","views":7099098,"poster":"Music.png","genre":4},{"filmid":19,"title":"Nebula","year":2020,"rating":"6.65","views":808540,"poster":"Nebula.png","genre":1},{"filmid":20,"title":"Neon","year":2018,"rating":"8.50","views":39704210,"poster":"Neon.png","genre":4},{"filmid":21,"title":"Network","year":2020,"rating":"5.95","views":481024,"poster":"Network.png","genre":5},{"filmid":22,"title":"Ocean","year":2020,"rating":"8.84","views":1080040,"poster":"Ocean.png","genre":1},{"filmid":23,"title":"Palm Trees","year":2019,"rating":"9.42","views":1430078,"poster":"Palm Trees.png","genre":1},{"filmid":24,"title":"Seoul","year":2017,"rating":"7.90","views":802379,"poster":"Seoul.png","genre":2},{"filmid":25,"title":"Sparrow","year":2016,"rating":"5.16","views":6095601,"poster":"Sparrow.png","genre":1},{"filmid":26,"title":"Trees","year":2016,"rating":"4.34","views":5006062,"poster":"Trees.png","genre":1},{"filmid":27,"title":"Wireframe","year":2016,"rating":"8.43","views":4290063,"poster":"Wireframe.png","genre":4},{"filmid":28,"title":"World","year":2017,"rating":"9.31","views":10450090,"poster":"World.png","genre":1}];
+				res.send(JSON.stringify(r)).end();
 			});
 		});
 	this.app.route('/search')
