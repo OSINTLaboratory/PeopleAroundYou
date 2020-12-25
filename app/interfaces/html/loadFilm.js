671d8b2a16a5f1f3711cc70d0d876f276988ffb4
Yulia02 <54748472+Yulia02@users.noreply.github.com>
2020-12-25 19:50:38 +0200

    Update loadFilm.js

--- app/interfaces/html/loadFilm.js
+++ app/interfaces/html/loadFilm.js
@@ -6,22 +6,21 @@ const id = parseInt(params.get("id"));
 
 
 async function getFilm(id) {
-    console.log(id);
-    let desc;
     await makeRequest(JSON.stringify({id}), "POST", "/getFilm").then( (res) => {
-        desc = JSON.parse(res);
-
-        const video = document.getElementById('filmPoster');
-        const source = document.createElement('source');
-        video.setAttribute('poster', `posters/${desc.poster}`);
-        source.setAttribute('src', `films/${desc.title}.mp4`);
-        video.appendChild(source);
-        video.load();
-
+        const desc = JSON.parse(res);
+        
+        desc.then(data => {
+            const video = document.getElementById('filmPoster');
+            const source = document.createElement('source');
+            video.setAttribute('poster', `posters/${data[0].poster}`);
+            source.setAttribute('src', `films/${data[0].title}.mp4`);
+            video.appendChild(source);
+            video.load();
+        });
+        
     }).catch((err) => {
         console.log(err.message);
-    });
-    return desc;
+    });    
 }
 
 document.addEventListener('DOMContentLoaded', () => {
