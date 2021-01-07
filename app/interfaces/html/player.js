07f923981636cbab229d4b42236301ed0725052c
Yulia02 <54748472+Yulia02@users.noreply.github.com>
2021-01-07 22:27:41 +0200

    Update player.js

--- app/interfaces/html/player.js
+++ app/interfaces/html/player.js
@@ -2,29 +2,23 @@
 let loginBtn1Showed = false;
 let registerBtnShowed = false;
 document.addEventListener("DOMContentLoaded", () => {
-	
-const commentField = document.getElementsByClassName('comment-field')[0];
-const commentButton = document.getElementsByClassName('comment-button')[0];
 
-let commentValue = '';
+	const commentField = document.getElementsByClassName('comment-field')[0];
+	const commentButton = document.getElementsByClassName('comment-button')[0];
 
-commentButton.addEventListener('click', function() {
-  commentValue = commentField.value;
+	const urlParams = new URLSearchParams(window.location.search);
+	const filmid = urlParams.get('id');
+	document.getElementById("film-id").value = filmid;
 
-	showFilms();
-	alert('lalka');
+	commentButton.addEventListener('click', () => {
+		const commentValue = commentField.value;
 
-//   async function postComment(id) {
-//     await makeRequest(JSON.stringify({id}), "POST", "/showComments").then( (res) => {
-//         const data = JSON.parse(res);
-        
-        
-//     }).catch((err) => {
-//         console.log(err.message);
-//     });    
-// }
-});
-	
+		makeRequest(JSON.stringify({"textdata":commentValue, "filmid": filmid}), "POST", "/addComment").catch((err) => {
+			console.log(err.message);
+		});
+
+	});
+
 	document.getElementById("login-btn-1").addEventListener("click", () => {
 		loginBtn1Showed = !loginBtn1Showed;
 		registerBtnShowed = false;
@@ -35,7 +29,7 @@ commentButton.addEventListener('click', function() {
 			HideLoginBtn1();
 		}
 	});
-	
+
 	document.getElementById("register-btn").addEventListener("click", () => {
 		registerBtnShowed = !registerBtnShowed;
 		loginBtn1Showed = false;
@@ -46,7 +40,7 @@ commentButton.addEventListener('click', function() {
 			HideRegisterBtn();
 		}
 	});
-	
+
 	document.getElementById("add-rating-form").addEventListener("submit", (event) => {
 		event.preventDefault();
 		const promise = makeRequest(`{"id":"${document.getElementById("film-id").value}","rating":"${event.target[2].value}"}`, "POST", '/addScore');
@@ -56,27 +50,48 @@ commentButton.addEventListener('click', function() {
 			}
 			res = JSON.parse(res);
 			document.getElementById("film-rating").innerHTML = `${res.rating} (${res.rating_count} оценок)`;
-			
+
 		}).catch((err)=>{});
 	});
-	
+
 	DisableAuth();
-		
-	const urlParams = new URLSearchParams(window.location.search);
-	const filmid = urlParams.get('id');
-	document.getElementById("film-id").value = filmid;
-	
+
 	const promise = makeRequest(`{"id":${filmid}}`, "POST", '/getFilmById');
 	promise.then( (res) => {
-		if(res === undefined){
+		if (res === undefined){
 			return;
 		}
 		res = JSON.parse(res);
 		document.getElementById("film-rating").innerHTML = `${res.rating} (${res.rating_count} оценок)`;
-		document.getElementById("film-name").innerHTML = `${res.title} `;
-		document.getElementById("film-genre").innerHTML = res.genre;
-		document.getElementById("film-year").innerHTML = `(${res.year})`;
-		
+		document.querySelector(".left-data").innerHTML = `${res.title}`;
+		document.querySelector(".genre").innerHTML = res.genre;
+		document.querySelector(".date").innerHTML = `(${res.year})`;
+
 	}).catch((err)=>{});
-	
-}, false);
+
+	const comments = makeRequest(`{"id":${filmid}}`, "POST", '/getCommentsForFilm');
+	comments.then( (res) => {
+		if(res === undefined){
+			return;
+		}
+		res = JSON.parse(res);
+
+		const commentsList = document.querySelector('.comment-list');
+
+		for (let i = 0; i < res.length; i++) {
+			makeRequest(`{"id":${res[i].userid}}`, "POST", '/getUsername').then(username => {
+				username = JSON.parse(username)[0].login;
+
+				const comment = document.createElement('div');
+				comment.innerHTML = `
+					<div class="comment">
+						<h5>${username}</h5>
+						<p>${res[i].textdata}</p>
+					</div>`;
+				commentsList.appendChild(comment);
+			});
+		}
+
+	}).catch((err) => {});
+
+}, true);
