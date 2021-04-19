22249a5b03b58c87e6116d09ea82b75e079b6a0f
imnetcat <guskov.danil@gmail.com>
2021-04-20 02:06:41 +0300

    Dockerize!

--- db/setup.sh
+++ db/setup.sh
@@ -1,4 +1,4 @@
-sudo -u postgres psql -c "CREATE USER \"$USER\" WITH PASSWORD '$USER';"
-sudo -u postgres psql -c "CREATE DATABASE application OWNER \"$USER\";"
+su - postgres psql -c "CREATE USER \"$USER\" WITH PASSWORD '$USER';"
+su - postgres psql -c "CREATE DATABASE application OWNER \"$USER\";"
 psql -d application -f install.sql
 
